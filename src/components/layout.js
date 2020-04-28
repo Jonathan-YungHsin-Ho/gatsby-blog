8bbaed637eb039917c2e842d2b4000527b453e5a
Jonathan Ho <johnnyho82@gmail.com>
2020-04-28 14:42:39 -0400

    Prepare for deploy

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './layout.module.scss'
+import Header from './header'
+import Footer from './footer'
+
+export default ({ children }) => (
+  <div className={styles.container}>
+    <Header />
+    <div className={styles.content}>{children}</div>
+    <Footer>My new Gatbsy Blog 2020</Footer>
+  </div>
+)
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Title from '../components/title'
-import ArticleList from '../components/article-list'
 
 export default () => (
   <Layout>
-    <Title text="Welcome" />
+    <Title text="About Me" />
+    <div>
+      <Link to="/">Home</Link> | <Link to="/about">About me</Link>
+    </div>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo minima
       dignissimos aspernatur corrupti obcaecati totam asperiores fuga harum
       soluta quod adipisci et, deleniti esse eum laudantium amet vero corporis
       dolore.
     </p>
-    <ArticleList />
   </Layout>
 )
